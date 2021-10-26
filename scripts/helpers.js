b381cb269b88ac23c68976c0d51a3f5ed191efcf
Hsiang Chou <champagne0906@gmail.com>
2021-10-26 17:05:40 +0800

    Add after_generate

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -1,3 +1,12 @@
-hexo.extend.helper.register('page_url', function(path, options) {
-    return this.url_for(path, options).replace(/index\.html$/, '');
+hexo.extend.helper.register("page_url", function (path, options) {
+  return this.url_for(path, options).replace(/index\.html$/, "");
+});
+
+hexo.extend.filter.register("after_generate", function () {
+  hexo.locals
+    .get("pages")
+    .filter((page) => page.layout == "false")
+    .forEach((page) => {
+      hexo.route.remove(page.path);
+    });
 });
